50416028e56c8d8ecde70431870b1c6cbdbf004a
hnnav <57915266+hnnav@users.noreply.github.com>
2022-11-17 10:26:24 +0200

    delete from frontend

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function TodoList() {
 
@@ -25,15 +26,22 @@ function TodoList() {
       })
       .catch((error) => console.log(error))
     }, [items]);
+
+    // Delete
+    const handleDelete = async (id) => {
+        await axios.delete(`http://localhost:8080/api/items/${id}`)
+        .then(console.log('deleted'))
+        .catch((error) => console.log(error))
+      };
   
     return (
         <div className="todo-list">
             <div className="all-items">
                 {items.map((item) => {
-                    return <div className="item">
+                    return <div key={item.id} className="item">
                         <img className="circle" src="\images\gray-circle-outline-png.png" alt="circle"></img>
-                        <p key={item.id}>{item.content}</p>
-                        <img src="\images\icon-cross.svg" alt="cross"></img>
+                        <p> {item.content} </p>
+                        <img className="cross" src="\images\icon-cross.svg" alt="cross" onClick={() => handleDelete(item.id)}></img>
                     </div>
                 })}
             </div>
