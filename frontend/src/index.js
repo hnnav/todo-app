af4e23175b1ff11c291ebe6746758adc8ddbf232
hnnav <57915266+hnnav@users.noreply.github.com>
2022-11-22 11:14:16 +0200

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-// import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-// if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
